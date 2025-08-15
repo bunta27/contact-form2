6d9381cc91c69786c93d8cfd0363912f9f0b8e61
ユーザー名 <>
2025-08-16 01:35:51 +0900

    chore: php.ini の非推奨設定整理 / ルート修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\UserRegistrationController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [ContactController::class, 'index']);
+Route::post('/confirm', [ContactController::class, 'confirm'])->name('contacts.confirm');
+Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+Route::get('/thanks', fn () => view('thanks'))->name('contacts.thanks');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/register', [UserRegistrationController::class, 'create'])->name('register');
+Route::post('/register', [UserRegistrationController::class, 'store'])->name('register.store');
+
+Route::get('/login', fn () => view('auth.login'))->name('login');
+Route::post('/sessions', [LoginController::class, 'store'])->name('login.store');
+Route::post('/logout',   [LoginController::class, 'destroy'])->name('logout');
+
+Route::prefix('admin')->name('admin.')->middleware(['auth'])->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('contacts.index');
+    Route::get('/contacts/export', [AdminController::class, 'export'])->name('contacts.export');
+    Route::get('/contacts/{contact}', [AdminController::class, 'show'])->name('contacts.show');
+    Route::delete('/contacts/{contact}', [AdminController::class, 'destroy'])->name('contacts.destroy');
 });
