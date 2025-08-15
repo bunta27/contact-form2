07cd9a3ffd3bffae709ea81e12fd90c2a66ed6bd
ユーザー名 <>
2025-08-16 00:27:10 +0900

    Restore lost app files from dangling commit 579b1c2

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,8 +12,11 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            CategorySeeder::class,
+            ContactSeeder::class,
+        ]);
     }
 }
