248f1925e1df73a0c6d2ba4b7beae4234b9fcec2
ユーザー名 <>
2025-08-20 21:27:27 +0900

    内容更新

--- src/app/Models/Contact.php
+++ src/app/Models/Contact.php
@@ -27,17 +27,15 @@ class Contact extends Model
         $g = $this->gender;
 
         $map = [
-            // 英語コード
             'male'   => '男性',
             'female' => '女性',
             'other'  => 'その他',
-            // 日本語保存
             '男性'   => '男性',
             '女性'   => '女性',
             'その他' => 'その他',
         ];
 
-        return $map[$g] ?? (string) $g; // 未知の値でも落ちない
+        return $map[$g] ?? (string) $g;
     }
 
     public function category()
